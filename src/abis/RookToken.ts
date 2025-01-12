e56c0ded67b3545d188fcb101d11168cda4260fa
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-01-12 16:51:46 +0000

    Add missing folder

---
+++ src/abis/RookToken.ts
@@ -0,0 +1,17 @@
+export const tokenAbi = [
+  {
+    name: "balanceOf",
+    type: "function",
+    stateMutability: "view",
+    inputs: [{ name: "owner", type: "address" }],
+    outputs: [{ name: "balance", type: "uint256" }]
+  },
+  {
+    name: "decimals",
+    type: "function",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ name: "decimals", type: "uint8" }]
+  }
+];
+
